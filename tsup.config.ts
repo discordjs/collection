bd2fe2a47c38f634b0334fe6e89f30f6f6a0b1f5
iCrawl <buechler.noel@outlook.com>
2021-10-27 06:58:52 +0200

    fix: minification of names

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,8 @@ export const tsup: Options = {
 	entryPoints: ['src/index.ts'],
 	format: ['esm', 'cjs'],
 	minify: true,
+	// if false: causes Collection.constructor to be a minified value like: 'o'
+	keepNames: true,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2021',
